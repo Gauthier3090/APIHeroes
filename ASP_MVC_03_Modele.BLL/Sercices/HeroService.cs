a69685e881ed07100df7514a5ddfd0c10c122db2
Gauthier3090 <gauthier.pladet@gmail.com>
2022-06-10 14:08:03 +0200

    Add project files.

---
+++ ASP_MVC_03_Modele.BLL/Sercices/HeroService.cs
@@ -0,0 +1,56 @@
+﻿using ASP_MVC_03_Modele.BLL.DTO;
+using ASP_MVC_03_Modele.BLL.Mappers;
+using ASP_MVC_03_Modele.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_MVC_03_Modele.BLL.Sercices
+{
+    public class HeroService
+    {
+        private IHeroRepository heroRepository;
+        public HeroService(IHeroRepository repo)
+        {
+            this.heroRepository = repo;
+        }
+
+        public IEnumerable<HeroDTO> GetAll()
+        {
+            return heroRepository.GetAll().Select(b => b.ToDTO());
+        }
+
+        public IEnumerable<HeroDTO> GetByName(string name)
+        {
+            return heroRepository.GetAll().Where(m => m.Name.ToLower().Contains(name)).Select(b => b.ToDTO());
+        }
+
+        public IEnumerable<HeroDTO> GetByEndurance(int endu)
+        {
+            return heroRepository.GetAll().Where(m => m.Endurance.Equals(endu)).Select(b => b.ToDTO());
+        }
+
+        public bool Insert(HeroDTO h)
+        {
+            return heroRepository.Insert(h.ToEntity()) > 0;
+        }
+
+        public bool Update(int id, HeroDTO h)
+        {
+            return heroRepository.Update(id, h.ToEntity());
+        }
+
+        public bool UpdateByName(int id, string name)
+        {
+
+            return heroRepository.UpdateByName(id, name);
+        }
+
+        public bool Delete(int id)
+        {
+            return heroRepository.Delete(id);
+        }
+    }
+}
---
+++ WEB_API_Heroes/Controllers/HeroesController.cs
@@ -0,0 +1,83 @@
+﻿using ASP_MVC_03_Modele.BLL.Sercices;
+using Microsoft.AspNetCore.Mvc;
+using WEB_API_Heroes.Mappers;
+using WEB_API_Heroes.Models;
+
+namespace WEB_API_Heroes.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HeroesController : ControllerBase
+{
+    private readonly HeroService _repo;
+
+    public HeroesController(HeroService repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        //renvoie une 200 + json
+
+        return Ok(_repo.GetAll().ToArray());
+    }
+
+    [HttpGet]
+    [Route("{name}")]
+    public IActionResult GetByName(string name)
+    {
+        //renvoie une 200 + json
+
+        return Ok(_repo.GetByName(name).Select(m => m.ToApi()));
+    }
+
+    [HttpGet]
+    [Route("{endurance:min(1)}")]
+    public IActionResult GetByEndurance(int endurance)
+    {
+        //renvoie une 200 + json
+
+        return Ok(_repo.GetByEndurance(endurance).Select(m => m.ToApi()));
+    }
+
+    [HttpPost]
+    public IActionResult AddHero(HeroApiModel monHero)
+    {
+        if (_repo.Insert(monHero.ToDto()))
+            return Ok(monHero);
+        return new BadRequestObjectResult(monHero);
+    }
+    [HttpPut]
+    public IActionResult UpdateHero(int id, HeroApiModel hero)
+    {
+        if (_repo.Update(id, hero.ToDto()))
+            return Ok(hero);
+        return new BadRequestObjectResult(hero);
+    }
+
+    [HttpPatch]
+    [Route("{id:int:min(1)}/{name:alpha}")]
+    public IActionResult UpdateHeroByName(int id,string name)
+    {
+        if (_repo.UpdateByName(id, name))
+        {
+            HeroApiModel hero = _repo.GetAll().First(m => m.IdHero == id).ToApi();
+            return Ok(hero);
+        }
+            
+        return new BadRequestObjectResult(new
+        {
+            id, name
+
+        });
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult DeleteHero(int id)
+    {
+        return Accepted(_repo.Delete(id));
+    }
+}
